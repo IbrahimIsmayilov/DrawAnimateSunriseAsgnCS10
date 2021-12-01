1a94728a0385d228b44c1e16ee5fa53469f8036a
IbrahimIsmayilov <90726322+IbrahimIsmayilov@users.noreply.github.com>
2021-12-01 14:49:00 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -6,27 +6,46 @@ let ctx = cnv.getContext("2d");
 cnv.width = 400;
 cnv.height = 400;
 
-// Store imgs in variables
+// Global Variables
 let cloudImg = document.getElementById("cloudImg");
+let x1Cloud = 130;
+let x2Cloud = 170;
+let r = 20;
+let xSun
 
-// Drawing stuff using ctx
+requestAnimationFrame(sunrise);
 
-// Drawing sky
-ctx.fillStyle = "blue";
-ctx.fillRect(0, 0, 400, 300); 
+function sunrise() {
+    // Updating Elements
+    // Animate until the clouds are off the canvas 
+    if (x1 > -100) {
+        x1 -= 5
+    }
 
-// Drawing sun
-ctx.fillStyle = "red";
-ctx.beginPath()
-ctx.arc(200, 300, 20, 0, 2 * Math.PI);
-ctx.fill()
+    if (x2 < 400) {
+        x2 += 5
+    }
 
-// Drawing grass
-ctx.fillStyle = "green";
-ctx.fillRect(0, 300, 400, 100); 
+    if (r < 50) {
+        r += 1;
+    }
+    
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, 400, 300); // sky 
 
-// Drawing cloud 
-ctx.drawImage(cloudImg, 130, 125)
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(200, 300, r, 0, 2 * Math.PI);
+    ctx.fill(); // sun
 
-// Drawing cloud 
-ctx.drawImage(cloudImg, 170, 100)
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 300, 400, 100); // grass 
+
+
+    ctx.drawImage(cloudImg, x1Cloud, 125) // cloud
+
+    ctx.drawImage(cloudImg, x2Cloud, 100) // cloud
+
+    requestAnimationFrame(sunrise);
+
+}
